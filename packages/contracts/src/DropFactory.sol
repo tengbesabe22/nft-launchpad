f74e7a2287fe9bb43142f57a37fa3550f3b7fdc9
Teng Besabe <tengbesabe@Tengs-MacBook-Air-2.local>
2025-02-24 21:24:07 +0800

    drop contracts

---
+++ packages/contracts/src/DropFactory.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.22;
+
+import {Drop} from "./Drop.sol";
+import {Clones} from "@openzeppelin/contracts/proxy/Clones.sol";
+
+contract DropFractory {
+    event DropCrafted(Drop indexed drop, address indexed owner);
+    Drop dropImpl;
+
+    constructor (Drop dropImpl_) {
+        dropImpl = dropImpl_;
+    }
+
+    function craftDrop (bytes32 salt, string memory name, string memory symbol) external returns (Drop drop) {
+        drop = Drop(predictDropAddress(salt));
+
+        emit DropCrafted(drop, msg.sender);
+
+        Clones.cloneDeterministic(address(dropImpl), salt);
+
+        drop.initialize(msg.sender, name, symbol);
+    }
+
+    function predictDropAddress(bytes32 salt_) public view returns (address drop) {
+        return Clones.predictDeterministicAddress(address(dropImpl), salt_);
+    }
+}
